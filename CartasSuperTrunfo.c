897cca0de6a5fec247bdf57f8615393243d52f37
Clayton Oliveira Silva <167586949+tommy2022br@users.noreply.github.com>
2025-07-29 03:52:47 +0000

    Printf retorn 0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,18 +3,28 @@
     int main() {
         printf("Desafio Super Trunfo! \n");
          
-        char Estado [10];
-        char Codigo [4];       
-        char Cidade [50];
+        char Estado, Bahia [10];
+        char Codigo, A01 [4];       
+        char Cidade, Salvador [50];
         int Populacao = 14.850;
         float Area = 693;
         float Pib = 62.9;
         int Pontosturisticos = 50;
 
+        char Estado, SaoPaulo [10];
+        char Codigo, A02 [5];       
+        char Cidade, Embu [50];
+        int Populacao = 250.691;
+        float Area = 70.398;
+        float Pib = 14.314;
+        int Pontosturisticos = 35;
+
+
+
         printf("\nDigite o Estado (uma letra de A a H): ");
         scanf(" %c", &Estado);
 
-        printf("Digite o código da carta, letra do estado seguida de um número de 01 a 04: ");
+        printf("Digite o código da carta: ");
         scanf(" %s\n", &Codigo);
 
         printf("Digite o nome da cidade: ");
@@ -32,6 +42,21 @@
         printf("Pontosturisticos: \n");
         scanf("%d\n", &Pontosturisticos);
 
+        printf("Carta 1! \n");
+        printf("Estado: %s \n", Estado);
+        printf("Codigo da Carta: %s \n", Estado, Codigo);
+        printf("Cidade: %s \n", Cidade);
+        printf("Populacao: %f \n", Populacao);
+        printf("Area: %f \n", Area);
+        printf("Pib: %f \n", Pib);
+
+        printf("Carta 2! \n");
+        printf("Estado: %s \n", Estado);
+        printf("Codigo da Carta: %s \n", Estado, Codigo);
+        printf("Cidade: %s \n", Cidade);
+        printf("Populacao: %f \n", Populacao);
+        printf("Area: %f \n", Area);
+        printf("Pib: %f \n", Pib);
 
   
 
