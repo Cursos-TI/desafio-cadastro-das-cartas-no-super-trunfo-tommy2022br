ff2d7dff7b5351bcfe2748628d95ef822a04b838
Clayton Oliveira Silva <167586949+tommy2022br@users.noreply.github.com>
2025-07-29 03:31:45 +0000

    Printf pontos turisticos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,13 +2,38 @@
     
     int main() {
         printf("Desafio Super Trunfo! \n");
+         
+        char Estado [10];
+        char Codigo [4];       
+        char Cidade [50];
+        int Populacao = 14.850;
+        float Area = 693;
+        float Pib = 62.9;
+        int Pontosturisticos = 50;
 
-        int 
+        printf("\nDigite o Estado (uma letra de A a H): ");
+        scanf(" %c", &Estado);
 
-   
+        printf("Digite o código da carta, letra do estado seguida de um número de 01 a 04: ");
+        scanf(" %s\n", &Codigo);
+
+        printf("Digite o nome da cidade: ");
+        scanf(" %s\n", &Cidade);
+
+        printf("Digite a população da cidade: ");
+        scanf(" %d\n", &Populacao);
+
+        printf("Area: \n");
+        scanf("%f\n", &Area);
+
+        printf("Pib: \n");
+        scanf("%f\n", &Pib);      
+
+        printf("Pontosturisticos: \n");
+        scanf("%d\n", &Pontosturisticos);
 
 
-
+  
 
 
 
